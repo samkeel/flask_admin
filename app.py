ad5b242a1380f023a1f7850c51b1bbc999a0ed9b
Vectors <programming@vectors.net.au>
2018-06-15 20:38:52 +0800

    project tidy up

--- app.py
+++ app.py
@@ -22,5 +22,10 @@ def drawinglist():
     return render_template("drawinglist.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('pages-404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
