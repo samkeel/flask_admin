1b9870e4339b6789cf952dcad03d12c11f16f1c9
Vectors <programming@vectors.net.au>
2018-07-05 22:10:02 +0800

    new pages added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import (Flask,
 
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker, scoped_session
-from database_setup import Base, Documents, DocTitles
+from database_setup import Base, Documents
 from flask_restful import Api
 
 
@@ -22,8 +22,6 @@ engine = create_engine('postgresql://testuser:test123@localhost/docs')
 Base.metadata.bind = engine
 api = Api(app)
 
-# api.add_resource(Stats, '/doccount')
-
 session = scoped_session(sessionmaker(bind=engine))
 
 
@@ -34,37 +32,25 @@ def home():
 
 @app.route("/dashboard")
 def dashboard():
-    recentdocs = session.query(Documents).order_by(desc(Documents.pub_update)).limit(5).all()
-    return render_template("dashboard.html", recentdocs=recentdocs)
+    return render_template("dashboard.html")
 
 
 @app.route("/drawinglist")
 def drawinglist():
-    doc_list = session.query(Documents).all()
-    return render_template("drawinglist.html", doc_list=doc_list)
-
-
-@app.route("/drawinglist/JSON")
-def drawinglistJSON():
-    doc_list = session.query(Documents).all()
-    return jsonify(Documents=[i.serialize for i in doc_list])
-
-@app.route("/drawinglist/JSON2")
-def drawinglisttitleJSON():
-    doctitles = session.query(DocTitles).all()
-    return jsonify(Documents=[i.serialize for i in doctitles])
+    # doc_list = session.query(Documents).all()
+    return render_template("drawinglist.html")
 
 
 @app.route("/newdocument", methods=['GET', 'POST'])
 def newdocument():
     if request.method == 'POST':
-        newDoc = Documents(project=request.form['newproj'],
-                           doc_name=request.form['newdoc'],
-                           revision=request.form['rev'])
-        session.add(newDoc)
-        new_title = DocTitles(title_line_1=request.form['tl1'])
-        session.add(new_title)
-        session.commit()
+        # newDoc = Documents(project=request.form['newproj'],
+        #                    doc_name=request.form['newdoc'],
+        #                    revision=request.form['rev'])
+        # session.add(newDoc)
+        # new_title = DocTitles(title_line_1=request.form['tl1'])
+        # session.add(new_title)
+        # session.commit()
         flash('Document Added.')
         return render_template("newdocument.html")
     else:
@@ -73,11 +59,11 @@ def newdocument():
 
 @app.route("/deldoc/<int:id>", methods=['GET', 'POST'])
 def deldoc(id):
-    docToDelete = session.query(Documents).filter_by(doc_id=id).first()
+    # docToDelete = session.query(Documents).filter_by(doc_id=id).first()
     if request.method == 'GET':
-        session.delete(docToDelete)
-        session.commit()
-        flash('Document deleted.')
+        # session.delete(docToDelete)
+        # session.commit()
+        # flash('Document deleted.')
         return redirect(url_for('drawinglist'))
 
 
@@ -86,14 +72,19 @@ def editdoc(id):
     # docToEdit = session.query(Documents).filter_by(doc_id=id).first()
     # docToEdit = session.query(DocTitles).filter_by(title_id=id).first()
     if request.method == 'GET':
-        return render_template('edit.html', docToEdit=docToEdit)
+        return render_template('edit.html')
 
 
-@app.route("/test")
-def test():
-    r = session.query(Documents).all()
-    dt = session.query(DocTitles).all()
-    return render_template("test.html", r=r, dt=dt)
+@app.route("/newdoc", methods=['GET', 'POST'])
+def newdoc():
+    if request.method == 'POST':
+        new_doc = Documents(doc_title=request.form['newtitle'])
+        session.add(new_doc)
+        session.commit()
+        flash('Document Added.')
+        return render_template('newdoc.html')
+    else:
+        return render_template('newdoc.html')
 
 
 
--- database_setup.py
+++ database_setup.py
@@ -7,58 +7,24 @@ from sqlalchemy.orm import backref, relationship
 
 Base = declarative_base()
 
+
 class Documents(Base):
     __tablename__ = 'documents'
 
-    doc_id = Column(Integer, primary_key=True, autoincrement=True)
-    project = Column(String(), nullable=True)
-    doc_name = Column(String(), nullable=False)
-    revision = Column(String(), nullable=False)
+    doc_id = Column(Integer, primary_key=True)
+    doc_title = Column(String(255), nullable=False)
+    doc_contents = Column(String(), nullable=True)
     pub_date = Column(DateTime, nullable=False, default=datetime.utcnow)
     pub_update = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
-    title_id = relationship('DocTitles', backref='abc', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Documents {}>'.format(self.body)
-
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'doc_id': self.doc_id,
-            'project': self.project,
-            'doc_name': self.doc_name,
-            'revision': self.revision,
-            'pub_date': self.pub_date,
-            'pub_update': self.pub_update
-            }
-
-
-class DocTitles(Base):
-    __tablename__ = 'doctitles'
-
-    _id = Column(Integer, primary_key=True, autoincrement=True)
-    title_line_1 = Column(String(), nullable=True)
-    title_line_2 = Column(String(), nullable=True)
-    title_line_3 = Column(String(), nullable=True)
-    title_line_4 = Column(String(), nullable=True)
-    title_id = Column(Integer, ForeignKey('documents.doc_id'))
-
-    def __repr__(self):
-        return '<DocTitles {}>'.format(self.body)
-
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            '_id': self._id,
-            'title_line_1': self.title_line_1,
-            'title_line_2': self.title_line_2,
-            'title_line_3': self.title_line_3,
-            'title_line_4': self.title_line_4
-            }
+    # @property
+    # def serialize(self):
+    #     """Return object data in easily serializeable format"""
+    #     return {
+    #         'doc_id': self.doc_id,
+    #         'doc_title': self.doc_title,
+    #         'doc_contents': self.doc_contents
+    #     }
 
 
 engine = create_engine('postgresql://testuser:test123@localhost/docs')
